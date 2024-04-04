00c835f7a4492e95011773ed85c9306ed13c965c
Levan Esitashvili <124806271+Lvnii@users.noreply.github.com>
2024-04-04 20:48:21 +0400

    commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -20,11 +20,26 @@ function App() {
 
     // console.log(obj)
 
-    const getFullName = (name: string, lastName: string, age?: number) => {
-      console.log(name + ' ' + lastName)
+    type Func = (name: string, lastName: string, age?: number) => string
+
+    let getFullName: Func;
+
+    getFullName = (name, lastName, age) => {
+      const full = `${name} ${lastName}`;
+      return full;
     }
 
-    getFullName('levani','esitashvili', 20)
+    type Gender = 'Male' | 'Female' | 'Other';
+
+    let gender : Gender = 'Male';
+    console.log(gender)
+
+    const flag: boolean = true
+
+
+    const fullName = getFullName('levani','esitashvili')
+
+
     
     type Person = {
       name: string,
@@ -41,29 +56,29 @@ function App() {
     const persons: Person[] = [person]
     // const persons: Array<Person> = [person]
 
-    console.log(persons)
+    // console.log(persons)
 
-    for (let i = 0; i < persons.length; i++) {
-      console.log(persons[i].lastName)
-    }
+    // for (let i = 0; i < persons.length; i++) {
+    //   console.log(persons[i].lastName)
+    // }
     
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
